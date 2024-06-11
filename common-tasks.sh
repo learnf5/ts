47da04c73cbc55d6689d32dc26ba26491cc9627c
djlamey <76072289+djlamey@users.noreply.github.com>
2024-06-11 11:24:50 -0400

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -2,16 +2,6 @@
 set -x
 PS4='+$(date +"%T.%3N"): '
 
-# get lab-info.md for student guide bravais id
-curl --silent https://raw.githubusercontent.com/learnf5/ts/main/lab-info.md --output /tmp/lab-info.md
-brav_id=$(awk -F '|' "/$LAB_ID/"' {print $2}' /tmp/lab-info.md)
-
-# install student guide
-#cat <<'EOF' >/home/student/Desktop/Lab_Guide.desktop
-
-# common files for all labs
-
-
 # run this lab's specific tasks saved on GitHub
 curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
 bash -x /tmp/$LAB_ID.sh
