9961b6f01020505287ff7b8c46fdf64dcbc5d2ab
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-24 14:55:03 -0400

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -2,6 +2,19 @@
 set -x
 PS4='+$(date +"%T.%3N"): '
 
+# set variable
+iApp=TS_iApp--station_v17.1.tmpl
+
+# confirm bigip1 is active
+for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+#touch /home/student/Desktop/bigip1_ready
+
+# download config from GitHub, copy to bigip1 and load/merge onto bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
+sudo scp /home/student/Downloads/$iApp 192.168.1.31:/shared/tmp
+sudo ssh 192.168.1.31 tmsh load /sys application template /shared/tmp/$iApp
+sudo ssh 192.168.1.31 tmsh save /sys config
+
 # run this lab's specific tasks saved on GitHub
 curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
 bash -x /tmp/$LAB_ID.sh
