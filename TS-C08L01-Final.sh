01a28ec8121faf7c21a55e43ae268f8c91b1b923
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-25 09:54:01 -0400

    Create TS-C08L01-Final.sh

---
+++ TS-C08L01-Final.sh
@@ -0,0 +1,14 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+#Download needed files
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/arp.sh --output /tmp/
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/fs.sh --output /tmp/
+
+# update Student Workstation
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# NOTHING specific for this lab - only the common lab files from common-tasks.sh
