00e3f96155f8053e62ca97e157aca27c6bf95197
djlamey <76072289+djlamey@users.noreply.github.com>
2025-04-16 08:48:32 -0400

    Update TS-UPGRADE.sh
    corrected curl commands for md5 files

--- TS-UPGRADE.sh
+++ TS-UPGRADE.sh
@@ -9,7 +9,10 @@ for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/TS_iApp--station_v15.1.tmpl --output /home/student/Downloads/TS_iApp--station_v15.1.tmpl
 rm /home/student/Downloads/TS_iApp--station_v17.1.tmpl
-curl --silent https://raw.githubusercontent.com/learnf5/ts/main/*.md5 --output /home/student/tmp/*.md5
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-16.1.5.2-0.0.5.iso.md5 --output /tmp/BIGIP-16.1.5.2-0.0.5.iso.md5
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.1.2.1-0.0.2.iso.md5 --output /tmp/BIGIP-17.1.2.1-0.0.2.iso.md5
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.5.0-0.0.15.iso.md5 --output /tmp/BIGIP-17.5.0-0.0.15.iso.md5
+scp /tmp
 
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
