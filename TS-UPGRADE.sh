9f82cae363417515fd453dc010a18a22200f4703
djlamey <76072289+djlamey@users.noreply.github.com>
2025-04-16 08:35:53 -0400

    Update TS-UPGRADE.sh
    added curl for md5 files

--- TS-UPGRADE.sh
+++ TS-UPGRADE.sh
@@ -2,16 +2,14 @@
 set -x
 PS4='+$(date +"%T.%3N"): '
 
-# set scf name
-iApp=TS_iApp--station_v15.1.tmpl
-
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 #touch /home/student/Desktop/bigip1_ready
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/TS_iApp--station_v15.1.tmpl --output /home/student/Downloads/TS_iApp--station_v15.1.tmpl
 rm /home/student/Downloads/TS_iApp--station_v17.1.tmpl
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/*.md5 --output /home/student/tmp/*.md5
 
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
