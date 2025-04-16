8235f5699214f39d2f476f2be43fbb6dd3d53005
djlamey <76072289+djlamey@users.noreply.github.com>
2025-04-16 07:54:31 -0400

    Update TS-UPGRADE.sh

--- TS-UPGRADE.sh
+++ TS-UPGRADE.sh
@@ -3,12 +3,15 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 # set scf name
-SCF=waf_rdp.scf
+iApp=TS_iApp--station_v15.1.tmpl
+
+# confirm bigip1 is active
+for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+#touch /home/student/Desktop/bigip1_ready
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$SCF --output /tmp/$SCF
-sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
-sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
+rm /home/student/Downloads/TS_iApp--station_v17.1.tmpl
 
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
