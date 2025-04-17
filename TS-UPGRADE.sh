c6ab8226e341e4fd0db3ccb74f3dda3de754e841
djlamey <76072289+djlamey@users.noreply.github.com>
2025-04-17 10:37:05 -0400

    Update TS-UPGRADE.sh
    added sudo to scp and ssh commands to bigip

--- TS-UPGRADE.sh
+++ TS-UPGRADE.sh
@@ -12,8 +12,8 @@ rm /home/student/Downloads/TS_iApp--station_v17.1.tmpl
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-16.1.5.2-0.0.5.iso.md5 --output /tmp/BIGIP-16.1.5.2-0.0.5.iso.md5
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.1.2.1-0.0.2.iso.md5 --output /tmp/BIGIP-17.1.2.1-0.0.2.iso.md5
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.5.0-0.0.15.iso.md5 --output /tmp/BIGIP-17.5.0-0.0.15.iso.md5
-scp /tmp/*.md5 192.168.1.31:/shared/images/
-ssh 192.168.1.31 rm /shared/images/BIGIP-17.1.1.1-0.0.2iso.md5
+sudo scp /tmp/*.md5 192.168.1.31:/shared/images/
+sudo ssh 192.168.1.31 rm /shared/images/BIGIP-17.1.1.1-0.0.2iso.md5
 
 
 # confirm bigip1 is active
